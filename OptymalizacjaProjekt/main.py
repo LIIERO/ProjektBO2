eede236a882ef2627bc29b50fc15e49409f1f0f6
Bartlomiej Matuszewski <bartekmatuzewski2001@gmail.com>
2022-12-06 21:44:41 +0100

    komentarze

--- OptymalizacjaProjekt/main.py
+++ OptymalizacjaProjekt/main.py
@@ -74,22 +74,31 @@ class FarmSimulation:
         for i in range(self.fieldNumber):
             plant = self.decisionMatrix[self.curr_year][i]
 
+            # aktualizacja jakości gleby na danym polu po uprawie rośliny
             if self.curr_year != 0:
                 self.Q[self.curr_year][i] = self.Q[self.curr_year - 1][i] - self.plantInfluenceDict[
                     self.decisionMatrix[self.curr_year - 1][i]]
+
+                # zabezpieczenie przed błednym indeksem
                 if self.Q[self.curr_year][i] < 0:
                     raise IndexError
+
+                # obliczanie przychodu, jeśli dwa razy nie uprawiamy to zerowy przychud
                 income = 0 if self.decisionMatrix[self.curr_year - 1][i] == plant == 'EMPTY' else (
                         self.fieldsSurfacesList[i] * self.earningsMatrix[plant][math.ceil(self.Q[self.curr_year][i])])
+
             else:
+                # obliczanie przychodu jeśli rok zerowy, nie trzeba sprawdzać poprzedniego
                 income = self.fieldsSurfacesList[i] * self.earningsMatrix[plant][math.ceil(self.Q[self.curr_year][i])]
 
-            expense = 0 if plant == 'EMPTY' else (
-                        self.productionCostDict[plant] * self.fieldsSurfacesList[i] + self.distanceMatrix[
-                    i] * self.transportCost)  # Jeśli nic nie siejemy to nie ponosimy kosztów
+            # obliczanie kosztów uprawy
+            expense = 0 if plant == 'EMPTY' else (self.productionCostDict[plant] * self.fieldsSurfacesList[i] +
+                                                  self.distanceMatrix[
+                                                      i] * self.transportCost)  # Jeśli nic nie siejemy to nie ponosimy kosztów
 
             self.earnings += income - expense
 
+        # następny rok, aktualizacja countera
         self.curr_year += 1
 
     def simulate_farm(self, decision_matrix_X: list[list]):  # Funkcja celu
@@ -98,7 +107,7 @@ class FarmSimulation:
         for y_dec in decision_matrix_X:
             self.__simulate_year_pass(y_dec)
 
-        return self.earnings # Ma zwracać rozwiązanie
+        return self.earnings  # Ma zwracać rozwiązanie
 
     def solve_greedy(self):  # Algorytm zachłanny - w każdym roku bierze to co da w nim największy zarobek
         self.__reset_variables()
@@ -124,8 +133,8 @@ class FarmSimulation:
                         elif y_dec == 0 or (y_dec > 0 and plant != self.decisionMatrix[y_dec - 1][no_field]):
                             plant_inc = (self.fieldsSurfacesList[no_field] * self.earningsMatrix[plant][
                                 math.ceil(pred_qual)]) - (
-                                                    self.productionCostDict[plant] * self.fieldsSurfacesList[no_field] +
-                                                    self.distanceMatrix[no_field] * self.transportCost)
+                                                self.productionCostDict[plant] * self.fieldsSurfacesList[no_field] +
+                                                self.distanceMatrix[no_field] * self.transportCost)
 
                         else:
                             plant_inc = -math.inf
@@ -138,33 +147,40 @@ class FarmSimulation:
             self.__simulate_year_pass(dec)
         return self.decisionMatrix
 
-    def simulated_annealing(self, s0: list[list], k_max): # Symulowane wyżarzanie
+    def simulated_annealing(self, s0: list[list], k_max):  # Symulowane wyżarzanie
+        # upewniamy się że zmienne są początkowe
         self.__reset_variables()
 
-        beast_s = deepcopy(s0)  # Rozwiązanie najlepsze
+        best_s = deepcopy(s0)  # Rozwiązanie najlepsze
         s = deepcopy(s0)  # Rozwiązanie początkowe
+
         for k in range(k_max):
+            # praypisywanie nowej temperatury co iteracje
             T = self.__annealing_temp(1 - ((k + 1) / k_max), k_max)
             s_new = self.__annealing_neig(s)
-            if self.simulate_farm(s_new) > self.simulate_farm(beast_s):
-                beast_s = s_new
+
+            if self.simulate_farm(s_new) > self.simulate_farm(best_s):
+                best_s = s_new
+
             if self.__annealing_P(self.simulate_farm(s), self.simulate_farm(s_new), T) >= random.uniform(0, 1):
                 s = deepcopy(s_new)
 
-        return beast_s
+        return best_s
 
     @staticmethod
-    def __annealing_temp(inp, k_m): # Funkcja obliczająca temperaturę
+    def __annealing_temp(inp, k_m):  # Funkcja obliczająca temperaturę
         if inp > 0:
-            return inp # Najprostszy sposób
+            return inp  # Najprostszy sposób
+
         else:
-            return 1/k_m
+            return 1 / k_m
 
-    def __annealing_neig(self, s_inp): # Funkcja wyznaczająca sąsiednie rozwiązanie
-
+    def __annealing_neig(self, s_inp):  # Funkcja wyznaczająca sąsiednie rozwiązanie
+        # w sposób pseudo losowy dobieramy rok i pole
         year = random.randrange(self.yearsNumber)
         field = random.randrange(self.fieldNumber)
         curr_plant = s_inp[year][field]
+        # losowe przyjęcie rośliny pod warunkiem że jest różna
         rand_plant = random.choice([plant for plant in PLANTS if plant != curr_plant])
 
         s_out = deepcopy(s_inp)
@@ -172,23 +188,28 @@ class FarmSimulation:
 
         # Zabezpieczenie przed wybraniem niedozwolonego rozwiązania
         # if rand_plant != 'EMPTY':
-            # if (year > 0 and s_inp[year - 1][field] == rand_plant) or (year < self.yearsNumber-1 and s_inp[year + 1][field] == rand_plant):
+        # if (year > 0 and s_inp[year - 1][field] == rand_plant) or (year < self.yearsNumber-1 and s_inp[year + 1][field] == rand_plant):
+
+        # try
         try:
             self.simulate_farm(s_out)
+
         except IndexError:
             # print('Nie spełnia ograniczenia jakości')
-            return self.__annealing_neig(s_inp) # Ponowna próba
+            return self.__annealing_neig(s_inp)  # Ponowna próba
+
         except ValueError:
             # print('Nie spełnia ograniczenia innej rośliny w każdym roku')
-            return self.__annealing_neig(s_inp) # Ponowna próba
+            return self.__annealing_neig(s_inp)  # Ponowna próba
 
         return s_out
 
     @staticmethod
-    def __annealing_P(e, e_dash, temp): # Funkcja akceptująca rozwiązanie, zmodyfikowana bo maksymalizujemy
-        if e_dash > e: return 1
-        else: return np.exp(((-1)*(e - e_dash))/temp)
-
+    def __annealing_P(e, e_dash, temp):  # Funkcja akceptująca rozwiązanie, zmodyfikowana bo maksymalizujemy
+        if e_dash > e:
+            return 1
+        else:
+            return np.exp(((-1) * (e - e_dash)) / temp)
 
 
 def main():
@@ -196,16 +217,17 @@ def main():
     T = 6 / 15 * 8 * 2 * 7.546  # spalanie na godzine/predkośc*ile razy trzeba pojechać * 2 * cena paliwa
 
     Cwheat = (87.43 + 19.76) * 2.5 + (87.43 + 27.56) * 2 + (87.43 + 18.80) * 2 + (
-                87.43 + 31.9) * 2.5 + 850 * 1.22 + 366 * 1.22 + 540 + 1458 + 1699 + 148 + 34
+            87.43 + 31.9) * 2.5 + 850 * 1.22 + 366 * 1.22 + 540 + 1458 + 1699 + 148 + 34
     Crye = (87.43 + 19.76) * 2 + (87.43 + 27.56) * 1.5 + (87.43 + 18.80) * 2 + (87.43 + 31.9) * 2.5 + 3000
     Cpotato = 2 * ((87.43 + 19.76) * 2.5 + (87.43 + 27.56) * 2 + (87.43 + 18.80) * 2 + (
-                87.43 + 31.9) * 2.5) + 17519.3412754
+            87.43 + 31.9) * 2.5) + 17519.3412754
     Ctriticale = (87.43 + 19.76) * 2.5 + (87.43 + 27.56) * 2 + (87.43 + 18.80) * 2 + (87.43 + 31.9) * 2.5 + 3573
     C = {'potato': Cpotato, 'wheat': Cwheat, 'rye': Crye, 'triticale': Ctriticale, 'EMPTY': 0}
 
     W = {'potato': 5, 'wheat': 8, 'rye': 3, 'triticale': 5, 'EMPTY': -5}
 
     G = {'potato': [], 'wheat': [], 'rye': [], 'triticale': [], 'EMPTY': []}
+
     for i in range(MQ):
         G['potato'].append(0 if i < 12 else (math.e ** ((i - 12) / 10) / 6002.91 * 17 + 24) * 710 + 558 + 1761.46)
         G['wheat'].append(0 if i < 35 else (math.e ** ((i - 35) / 10) / 601.845 * 10 + 3.9) * 1490 + 558)
